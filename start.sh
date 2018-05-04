da86181bd772c9af030b4594abe2164829877e3e
KappaBull <kappa8v11@gmail.com>
2018-05-04 23:48:15 +0900

    一応startの方でも宣言

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 . ./settings.sh
-"$JAVACMD" -server -Xms${MIN_RAM} -Xmx${MAX_RAM} ${JAVA_PARAMETERS} -jar ${FORGEJAR} nogui
+export MAX_RAM="7G"       # -Xmx
+export MIN_RAM="6G"       # -Xms
+"$JAVACMD" -server -Xmx${MAX_RAM} -Xms${MIN_RAM} ${JAVA_PARAMETERS} -jar ${FORGEJAR} nogui
